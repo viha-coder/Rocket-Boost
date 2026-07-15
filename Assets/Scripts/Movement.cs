0954a496937cf1d0d914531e95163a9a2f86ae5c
viha-coder <guilhermemedeiros2233@gmail.com>
2026-07-15 14:51:21 -0300

    First Commit
    -add .gitignore

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+ private void OnEnable()
+ {
+    thrust.Enable();
+ }
+
+private void Update()
+ {
+  if (thrust.IsPressed())
+   {
+      Debug.Log("give me some space");
+   }
+ }
+}
