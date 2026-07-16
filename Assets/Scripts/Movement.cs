5baf6b1f68d10176e207be6d6542ebd21aba3249
viha-coder <guilhermemedeiros2233@gmail.com>
2026-07-16 01:55:36 -0300

    Update 1.0
    -Add thrust and rotation to the movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,17 +4,52 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] private float thrustForce = 1000f;
+    [SerializeField] private float rotationForce = 100f;
+    Rigidbody rb;
 
- private void OnEnable()
+private void Start()
+{
+   rb = GetComponent<Rigidbody>();
+}    
+
+private void OnEnable()
  {
     thrust.Enable();
+    rotation.Enable();
  }
 
-private void Update()
+private void FixedUpdate()
  {
-  if (thrust.IsPressed())
-   {
-      Debug.Log("give me some space");
-   }
+      ProcessThrust();
+      ProcessRotation();
+ }
+
+private void ProcessThrust()
+ {
+    if (thrust.IsPressed())
+    {
+        rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+    }
+ }
+
+ private void ProcessRotation()
+ {
+  float rotationInput = rotation.ReadValue<float>();
+  if (rotationInput < 0f)
+      {
+         ApplyRotation(rotationForce);
+      }
+   else  if (rotationInput > 0f)
+      {
+         ApplyRotation(-rotationForce);
+      }
+
+  void ApplyRotation(float rotationThisFrame)
+      {
+         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+      }
  }
 }
+
